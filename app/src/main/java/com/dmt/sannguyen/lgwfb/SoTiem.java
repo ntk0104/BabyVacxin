bea9dae99041e60a5846d5abe6c88fff4e1761c0
Kiet Nguyen <roynguyen147@gmail.com>
2018-12-17 21:10:01 +0700

    add some activity

---
+++ app/src/main/java/com/dmt/sannguyen/lgwfb/SoTiem.java
@@ -0,0 +1,13 @@
+package com.dmt.sannguyen.lgwfb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SoTiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_so_tiem);
+    }
+}
---
+++ app/src/main/java/com/dmt/sannguyen/lgwfb/TramXa.java
@@ -0,0 +1,13 @@
+package com.dmt.sannguyen.lgwfb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TramXa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tram_xa);
+    }
+}
