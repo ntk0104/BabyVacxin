b62e18ccf8ab2201ca29c28b8fa84ea172448524
Roy Nguyen <roynguyen147@gmail.com>
2018-12-20 17:57:13 +0700

    add check existed user func

--- app/src/main/java/com/dmt/sannguyen/lgwfb/LoginActivity.java
+++ app/src/main/java/com/dmt/sannguyen/lgwfb/LoginActivity.java
@@ -6,6 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -17,10 +24,13 @@ import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.facebook.login.widget.ProfilePictureView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -56,7 +66,8 @@ public class LoginActivity extends AppCompatActivity {
                                 //Send data to another acitivity
                                 Bundle bundle = new Bundle();
                                 bundle.putString("userid", object.getString("id").toString());
-
+                                // Call API to check userid existed or not
+                                checkExistedUser(object.getString("id").toString());
 
                                 //send bundle data to BeYeuActivity activity
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
@@ -98,6 +109,37 @@ public class LoginActivity extends AppCompatActivity {
             Log.d("Track", e.toString());
         }
 
+    }
+
+    private void checkExistedUser(final String userid){
+        String url = "https://babyvacxin.azurewebsites.net/api/CheckUserExisted?code=1rM/4T2O/pALgGTBgnVQCDgFgLz1Dhz10ODeHFROkUjGHf3J401Nlw==";
+        Log.d("Track", "Checking User Existed");
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Track", "Response from checkExistedUser :" + response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Track", "Response from checkExistedUser :"+error);
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("username", userid);
+                return params;
+            }
+        };
+        requestQueue.add(jsonObjectRequest);
     }
 
 
