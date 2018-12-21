32ac35796271c0cb2e0791bb97688de8df2154e8
ntk0104 <roynguyen147@gmail.com>
2018-12-20 21:42:50 -0500

    added func checkexisted for user and register if not existed

--- app/src/main/java/com/dmt/sannguyen/lgwfb/LoginActivity.java
+++ app/src/main/java/com/dmt/sannguyen/lgwfb/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -39,6 +40,8 @@ public class LoginActivity extends AppCompatActivity {
     LoginButton loginButton;
     CallbackManager callbackManager;
 
+    public String USERID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,17 +66,8 @@ public class LoginActivity extends AppCompatActivity {
                         public void onCompleted(JSONObject object, GraphResponse response) {
                             Log.d("Track", object.toString());
                             try {
-                                //Send data to another acitivity
-                                Bundle bundle = new Bundle();
-                                bundle.putString("userid", object.getString("id").toString());
                                 // Call API to check userid existed or not
-                                checkExistedUser(object.getString("id").toString());
-
-                                //send bundle data to BeYeuActivity activity
-                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                intent.putExtras(bundle);
-                                startActivity(intent);
-
+                                checkExistedUser(object.getString("id"), object.getString("first_name"));
 
                             } catch (JSONException e) {
                                 e.printStackTrace();
@@ -111,7 +105,7 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void checkExistedUser(final String userid){
+    private void checkExistedUser(final String userid, final String first_name){
         String url = "https://babyvacxin.azurewebsites.net/api/CheckUserExisted?code=1rM/4T2O/pALgGTBgnVQCDgFgLz1Dhz10ODeHFROkUjGHf3J401Nlw==";
         Log.d("Track", "Checking User Existed");
         RequestQueue requestQueue = Volley.newRequestQueue(this);
@@ -123,6 +117,14 @@ public class LoginActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d("Track", "Response from checkExistedUser :" + response);
+                        try {
+                            String result_existed = response.getJSONArray("UserStatus").getJSONObject(0).getString("existed");
+                            Log.d("Track", "Response from checkExistedUser value :" + result_existed);
+                            AfterCheckLogin(result_existed, first_name, userid);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+//                        AfterCheckLogin(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -135,12 +137,79 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String>  params = new HashMap<String, String>();
-                params.put("username", userid);
+                params.put("userid", userid);
                 return params;
             }
+
+
         };
         requestQueue.add(jsonObjectRequest);
     }
+
+    private void RegisterNewUser(final String userid, final String first_name){
+        String url = "https://babyvacxin.azurewebsites.net/api/RegisterUser?code=CSSXSl6Vf86ljMtrQ90LbFnTNhxdyNtRKMXio7HEiBQf8LhXtdhDhA==";
+        Log.d("Track", "Registering new user");
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Track", "Response from RegisterNewUser :" + response);
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Track", "Response from RegisterNewUser :"+error);
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("userid", userid);
+                params.put("username", first_name);
+                return params;
+            }
+
+
+        };
+        //set to avoid Volley send request multiple times
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(0, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private void AfterCheckLogin(String response, String first_name, String userid) {
+        if (response.equals("true")){
+            Log.d("Track", "AfterCheckLogin: User da ton tai");
+            //Send data to another acitivity
+            Bundle bundle = new Bundle();
+            bundle.putString("userid", userid);
+            //send bundle data to BeYeuActivity activity
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+        }else {
+            Log.d("Track", "AfterCheckLogin: User chua ton tai");
+            Log.d("Track", "AfterCheckLogin: Dang Ki User moi vao db");
+            //Send data to another acitivity
+            Bundle bundle = new Bundle();
+            bundle.putString("userid", userid);
+            //send bundle data to BeYeuActivity activity
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+            RegisterNewUser(userid, first_name);
+        }
+
+    }
+
 
 
     private void anhXa() {
